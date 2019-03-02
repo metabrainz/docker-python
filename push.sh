1cd38825b5bf7353054fda4c1cb17bfa9d04c502
Param Singh <paramsingh258@gmail.com>
2019-03-02 14:15:53 +0530

    Remove python 3.5 from push.sh as well

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 # Build all Python images image from the currently checked out version and
 # push them it to the Docker Hub.
 
-for version in 2.7 3.5 3.6
+for version in 2.7 3.6
 do
 	pushd "$(dirname "${BASH_SOURCE[0]}")/${version}/"
 	echo "Building ${version}..."
