f5736905197a0149056c9d22f08f32ae4d16f8f8
yvanzo <yvanzo@metabrainz.org>
2019-12-26 20:39:05 +0100

    Add 3.8
    Based on the official Docker Python image at
    https://github.com/docker-library/python/blob/32b69d61f6/3.8/buster/Dockerfile
    in the same format as the existing metabrainz 3.6/3.7 images

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 # Build all Python images image from the currently checked out version and
 # push them it to the Docker Hub.
 
-for version in 2.7 3.6 3.7
+for version in 2.7 3.6 3.7 3.8
 do
 	pushd "$(dirname "${BASH_SOURCE[0]}")/${version}/"
 	echo "Building ${version}..."
