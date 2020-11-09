fdac3a838dc5e9abf9f5971278fd37f825472ac7
yvanzo <yvanzo@metabrainz.org>
2020-11-09 18:50:31 +0100

    Exit push script on any error

--- push.sh
+++ push.sh
@@ -3,6 +3,8 @@
 # Build all Python images image from the currently checked out version and
 # push them it to the Docker Hub.
 
+set -e -o pipefail -u
+
 for version in 2.7 3.6 3.7 3.8
 do
 	pushd "$(dirname "${BASH_SOURCE[0]}")/${version}/"
