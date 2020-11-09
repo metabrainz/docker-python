a43860e735e59530f3707f0142bf14692e7d9682
yvanzo <yvanzo@metabrainz.org>
2020-11-09 19:22:47 +0100

    Set image name in a variable for safety
    Script errors on unset variable, while it didn’t necessarily error on
    mistyped image name.

--- push.sh
+++ push.sh
@@ -5,15 +5,17 @@
 
 set -e -o pipefail -u
 
+image_name='metabrainz/python'
+
 DOCKER_CMD=${DOCKER_CMD:-docker}
 
 for version in 2.7 3.6 3.7 3.8
 do
 	pushd "$(dirname "${BASH_SOURCE[0]}")/${version}/"
 	echo "Building ${version}..."
-	${DOCKER_CMD} build -t metabrainz/python:${version} .
+	${DOCKER_CMD} build -t ${image_name}:${version} .
 	echo "Pushing ${version}..."
-	${DOCKER_CMD} push metabrainz/python:${version}
+	${DOCKER_CMD} push ${image_name}:${version}
 	popd
 done
 
